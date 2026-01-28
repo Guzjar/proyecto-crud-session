43f25126f8200919d66488021e2b9116a709f679
angel_santiago_guzman_jaramillo <anguja@alumnatflorida.es>
2026-01-28 20:11:25 +0100

    anadiendo

---
+++ models/modeloM.php
@@ -0,0 +1,11 @@
+<?php
+abstract class EntidadEstelar{
+    private $id;
+    private $nombre;
+    private $planetaDeOrigen;
+    private $nivelDeEstabilidad;
+
+    function public setid(){
+        
+    }
+}
