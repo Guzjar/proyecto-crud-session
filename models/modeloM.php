ec83c941d68f7d9fe80728cc586d4f774f7d99fa
angel_santiago_guzman_jaramillo <anguja@alumnatflorida.es>
2026-01-28 21:10:57 +0100

    modelomadre

--- models/modeloM.php
+++ models/modeloM.php
@@ -4,8 +4,13 @@ abstract class EntidadEstelar{
     private $nombre;
     private $planetaDeOrigen;
     private $nivelDeEstabilidad;
+    function public getid(){return $this->id;}
+    function public getnombre(){return $this->nombre;}
+    function public getplanetaDeOrigen(){return $this->planetaDeOrigen;}
+    function public getnivelDeEstabilidad(){return $this->nivelDeEstabilidad;}
 
-    function public setid(){
-        
-    }
+    function public setid($id){$this->id = $id;}
+    function public setnombre($nombre){$this->nombre = $nombre;}
+    function public setplanetaDeOrigen($planetaDeOrigen){$this->planetaDeOrigen = $planetaDeOrigen;}
+    function public setnivelDeEstabilidad($nivelDeEstabilidad){$this->nivelDeEstabilidad = $nivelDeEstabilidad;}
 }
